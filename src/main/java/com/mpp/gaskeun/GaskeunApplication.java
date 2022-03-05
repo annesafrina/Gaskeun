2403354fa4ffc2e1f5fb0214a194b715d44a2834
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-03-05 11:29:30 +0700

    Initial commit

---
+++ src/main/java/com/mpp/gaskeun/GaskeunApplication.java
@@ -0,0 +1,13 @@
+package com.mpp.gaskeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaskeunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaskeunApplication.class, args);
+    }
+
+}
