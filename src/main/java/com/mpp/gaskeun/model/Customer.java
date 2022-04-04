1dc570be6e4157f9209eb08e39b493f76cc94836
rashadaziz <rashad.aziz@ui.ac.id>
2022-04-04 14:49:44 +0700

    Implement Customer Model

--- src/main/java/com/mpp/gaskeun/model/Customer.java
+++ src/main/java/com/mpp/gaskeun/model/Customer.java
@@ -1,8 +1,11 @@
 package com.mpp.gaskeun.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
-
+@Getter @Setter
 @Entity
 @Table(name="customer")
 public class Customer {
