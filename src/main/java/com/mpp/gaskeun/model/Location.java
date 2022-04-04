d1e8af942e7b2a1f956e716e7bab4635d3d41930
johanes.christian <johanes.christian@ui.ac.id>
2022-04-04 14:07:04 +0700

    Implement location model
    Implemented location model and added JPA dependency and postgresql
    to communicate to the postgresql instance.

---
+++ src/main/java/com/mpp/gaskeun/model/Location.java
@@ -0,0 +1,15 @@
+package com.mpp.gaskeun.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Location")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "cityName")
+    private String cityName;
+}
