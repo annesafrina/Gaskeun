a574df999fc3da76308f7910330d2892d55f0f02
Anne.yuliana <anne.yuliana@ui.ac.id>
2022-04-09 13:26:03 +0700

    Create interface for ReviewService service

---
+++ src/main/java/com/mpp/gaskeun/service/ReviewService.java
@@ -0,0 +1,12 @@
+package com.mpp.gaskeun.service;
+
+import com.mpp.gaskeun.model.*;
+
+import java.util.Date;
+
+public interface ReviewService {
+    public boolean validateOrderIsCompleted(Customer customer, Order order);
+    public CarReview submitCarReviewAndRating(Customer customer, Order order, CarReview car_review);
+    public ProviderReview submitProviderReviewAndRating(Customer customer, Order order, ProviderReview provider_review);
+    public CustomerReview submitCustomerReviewAndRating(Provider provider, Order order, CustomerReview customer_review);
+}
