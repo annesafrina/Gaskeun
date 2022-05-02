637bf84a9ee2813910836c6d6109f3cadb7852f5
johanes.christian <johanes.christian@ui.ac.id>
2022-05-03 00:13:56 +0700

    Describe method for testing purposes

--- src/main/java/com/mpp/gaskeun/service/OrderServiceImpl.java
+++ src/main/java/com/mpp/gaskeun/service/OrderServiceImpl.java
@@ -1,2 +1,52 @@
-package com.mpp.gaskeun.service;public class OrderServiceImpl {
+package com.mpp.gaskeun.service;
+
+import com.mpp.gaskeun.model.*;
+
+import java.util.Date;
+
+public class OrderServiceImpl implements OrderService{
+    @Override
+    public boolean validateOrder(Customer customer, Car car, Date startDate, Date endDate) {
+        return false;
+    }
+
+    @Override
+    public Order createOrder(Customer customer, Car car, Date startDate, Date endDate, String pickupLocation, String dropoffLocation) {
+        return null;
+    }
+
+    @Override
+    public void cancelOrder(Customer customer, Order order) {
+
+    }
+
+    /**
+     * Method to change the status and booking message of an order. This process is conducted by a rental provider.
+     * Hence, before making any changes to the Order object, we first verify if the order indeed belongs to the
+     * provider.
+     *
+     * If verified, the changes will be made and committed.
+     *
+     * @param provider=The provider object that receives the order from the customer
+     * @param order=The order whose status and description is wanted to be changed
+     * @param status=The status given by the rental provider to the order
+     * @param bookingMessage=Description given by the rental provider regarding the booking/order
+     *                      The field is nullable.
+     * @return The edited order
+     */
+    @Override
+    public Order confirmOrRejectOrder(RentalProvider provider, Order order, OrderStatus status, String bookingMessage) {
+        return null;
+    }
+
+
+    /**
+     * Verifies whether an order is destined to a rental provider
+     * @param provider=The provider whose ownership over an order is to be verified
+     * @param order=The order object to be tested
+     * @return true if the car used in the order belongs to the provider, false if otherwise
+     */
+    private boolean verifyOrderOwnership(RentalProvider provider, Order order) {
+        return false;
+    }
 }
--- src/main/java/com/mpp/gaskeun/service/SearchServiceImpl.java
+++ src/main/java/com/mpp/gaskeun/service/SearchServiceImpl.java
@@ -1,4 +1,35 @@
 package com.mpp.gaskeun.service;
 
-public class SearchServiceImpl {
+import com.mpp.gaskeun.model.*;
+
+import java.util.List;
+
+public class SearchServiceImpl implements SearchService{
+
+    /**
+     * Method to return all cars in the database provided by any car provider
+     * @return List of all cars provided in the database
+     * */
+    @Override
+    public List<Car> getCars() {
+        return null;
+    }
+
+    /**
+     * @param location=The required location (city) where the cars should be stationed
+     * @param dateRange=The required date range where the car should be available: The car should not be part of any order
+     *                 in this date range (orderStartDate <= endDate and orderEndDate >= startDate)
+     * @param carCapacity=Required car capacity
+     * @param transmission=Required car transmission
+     * @param priceRange=The price of the car must be within this range (priceRange.startPrice <= price <= priceRange.endPrice)
+     * @return A list of cars matching the required filter.
+     *
+     * Note: It is unnecessary for all field to be filled. If the value of a field is null (not provided by user), the field
+     * will be neglected in the filtration process. That is, all cars that corresponds to any of the possible values of the
+     * neglected field will be included as long as it complies to the remaining filters.
+     */
+    @Override
+    public List<Car> getCars(Location location, DateRange dateRange, int carCapacity, Transmission transmission, PriceRange priceRange) {
+        return null;
+    }
 }
