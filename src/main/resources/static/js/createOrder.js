3a532cf6fa6f7f405a3a6d7da92c3b70286ab746
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-05-26 00:59:08 +0700

    Style create order page

---
+++ src/main/resources/static/js/createOrder.js
@@ -0,0 +1,26 @@
+const priceSpan = document.querySelector(".price-show__price");
+const hiddenInput = document.querySelector("#price-per-day");
+
+const startDate = document.querySelector("#start-date");
+const endDate = document.querySelector("#end-date");
+
+const changePriceDisplay = (e) => {
+  const startDateValue = startDate.value;
+  const endDateValue = endDate.value;
+
+  if (startDateValue == "" || endDateValue == "") return;
+
+  const startDateDate = new Date(startDateValue);
+  const endDateDate = new Date(endDateValue);
+
+  const pricePerDay = Number(hiddenInput.value);
+  const differentDays = Math.ceil(
+    (endDateDate.getTime() - startDateDate.getTime()) / (1000 * 24 * 3600)
+  );
+
+  const totalPrice = pricePerDay * differentDays;
+  priceSpan.innerText = Intl.NumberFormat("id-ID").format(totalPrice);
+};
+
+startDate.addEventListener("change", changePriceDisplay);
+endDate.addEventListener("change", changePriceDisplay);
