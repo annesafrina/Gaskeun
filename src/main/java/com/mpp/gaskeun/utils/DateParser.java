6d46faab5fc90f73da697557f0378272f11e16a4
johanes.christian <johanes.christian@ui.ac.id>
2022-05-27 16:26:17 +0700

    Remove unused imports in DateParser

--- src/main/java/com/mpp/gaskeun/utils/DateParser.java
+++ src/main/java/com/mpp/gaskeun/utils/DateParser.java
@@ -1,15 +1,14 @@
 package com.mpp.gaskeun.utils;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class DateParser {
     private DateParser() {
         throw new IllegalStateException("Utility class");
     }
+
     public static String parse(Date date) {
         String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-        return String.format("%d %s %d", date.getDate(), months[date.getMonth()], date.getYear()+1900);
+        return String.format("%d %s %d", date.getDate(), months[date.getMonth()], date.getYear() + 1900);
     }
 }
