259cf024ab17a6ebb9c0968d57ce9872e57286eb
rashadaziz <rashad.aziz@ui.ac.id>
2022-05-28 00:56:48 +0700

    Fix api endpoint

--- src/main/java/com/mpp/gaskeun/security/config/WebSecurityConfig.java
+++ src/main/java/com/mpp/gaskeun/security/config/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/actuator/*",
                         "/",
                         "/explore",
-                        "/explore/*",
+                        "/explore/**",
                         "/static/**"
                 )
                 .permitAll()
