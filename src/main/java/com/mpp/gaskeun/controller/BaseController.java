92e290030b2d4c8f42b57d212e855fac204477c4
rashadaziz <rashad.aziz@ui.ac.id>
2022-05-27 19:21:08 +0700

    Refactor home page

--- src/main/java/com/mpp/gaskeun/controller/BaseController.java
+++ src/main/java/com/mpp/gaskeun/controller/BaseController.java
@@ -10,10 +10,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@Controller @Slf4j
+@Controller
+@Slf4j
 @RequestMapping("/")
 public class BaseController {
 
